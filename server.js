94ea2f8ef2d27b64661a905fc57e6a218e64b306
d1son <dson120@gmail.com>
2016-02-22 14:57:20 -0500

    created the handlebar setup, created views folder, and layouts folder, created main.handlebars

--- server.js
+++ server.js
@@ -14,5 +14,21 @@ var connection = new Sequelize("login_auth_db", "root");
 var bcrypt = require("bcryptjs"); //needed to encrypt password
 var bodyParser = require("body-parser");
 
+//Middleware code
+app.use(require("express-session")({ //using express sessions in order to have the user logged in for a certain period of time
+	secret: "blackmamba",
+	resave: true, // unsure of what this line does
+	saveUnitialized: true, //unsure of what this line does
+	cookie : { secure : false, maxAge : (4 * 60 * 60 * 1000) }//4 hours
+}));
+
+// app.use(passport.initialize()); this is for using passport
+// app.use(passport.session()); this is for using passport
+
+// Handlebars setup, expressHandlebars initialized at the top of the page
+
+app.engine("handlebars", expressHandlebars({
+	defaultlayout: "main" //default layout is set to the "main" file, in the layouts folder
+}));
 
 
