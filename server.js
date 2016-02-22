67ec420ad2f3a90047ee8d9683b5b3c6fd4c8276
d1son <dson120@gmail.com>
2016-02-22 15:27:50 -0500

    set up the routes for the home and registration page, got the pages to render

--- server.js
+++ server.js
@@ -9,18 +9,20 @@ var expressHandlebars = require("express-handlebars");
 //initialize Database setup
 var Sequelize = require("sequelize");
 var connection = new Sequelize("login_auth_db", "root");
+var mysql = require("mysql");
+
 
 //initialize other packages needed
-var bcrypt = require("bcryptjs"); //needed to encrypt password
+// var bcrypt = require("bcryptjs"); //needed to encrypt password
 var bodyParser = require("body-parser");
 
 //Middleware code
-app.use(require("express-session")({ //using express sessions in order to have the user logged in for a certain period of time
-	secret: "blackmamba",
-	resave: true, // unsure of what this line does
-	saveUnitialized: true, //unsure of what this line does
-	cookie : { secure : false, maxAge : (4 * 60 * 60 * 1000) }//4 hours
-}));
+// app.use(require("express-session")({ //using express sessions in order to have the user logged in for a certain period of time
+// 	secret: "blackmamba",
+// 	resave: true, // unsure of what this line does
+// 	saveUnitialized: true, //unsure of what this line does
+// 	cookie : { secure : false, maxAge : (4 * 60 * 60 * 1000) }//4 hours
+// }));
 
 // app.use(passport.initialize()); this is for using passport
 // app.use(passport.session()); this is for using passport
@@ -31,4 +33,24 @@ app.engine("handlebars", expressHandlebars({
 	defaultlayout: "main" //default layout is set to the "main" file, in the layouts folder
 }));
 
+app.set("view engine", "handlebars"); //not sure what this does
 
+app.use(bodyParser.urlencoded({ //uses the body parser to get info from the body
+	extended: false
+}));
+
+// Setup the routes for each of the pages that need to be rendered
+
+app.get("/", function (req, res) {
+	res.render("index"); // the file in the ("") must be in the views folder
+});
+
+app.get("/registration", function(req, res) {
+	res.render("registration", { msg: "TEST MESSAGE"}); //this will render the registration.handlebars in the views folder
+});
+
+connection.sync().then(function() {
+	app.listen(PORT, function () {
+		console.log("Listening on port: " + PORT);
+	})
+});
